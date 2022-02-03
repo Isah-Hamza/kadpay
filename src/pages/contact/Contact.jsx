ed2929bc97f540c8cdea1576b953d5542ea203c5
Isah Hamza <hamza@paritie.com>
2022-02-03 15:17:52 +0100

    versioning untracked files

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './contact.css';
+
+function Contact() {
+  return <div className='contact-container'>
+            <div class="contact">
+                <h1>KadPay contact <span className='primary-color'>page</span> </h1>
+                <p class="enquiry">For inquiries, suggestions and complains. Mail us at: </p>
+                <p class="email">info@kadpay@yahoo.com</p>
+                <div class="subject">
+                    <p>Subject <span>*</span></p>
+                    <input type="text" name="subject" id="subject" />
+                </div>
+                <div class="mesage">
+                    <p>Message <span>*</span></p>
+                    <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                </div>
+                <button class="send">Send</button>
+          </div>
+  </div>;
+}
+export default Contact;
