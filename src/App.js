ef9ea624016b4cf0567760f31f528c68d54f251b
Isah Hamza <hamza@paritie.com>
2022-02-03 12:43:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import Footer from "./components/footer/Footer";
-import Header from "./components/header/Header";
-import Main from "./components/main/Main";
+import { Route, BrowserRouter as Router , Link, Switch } from 'react-router-dom'
+import Header from './components/header/header.jsx'
+import Landing from './pages/landing/landing.jsx';
+import Footer from './components/footer/footer.jsx';
+import Contact from './pages/contact/contact.jsx';
 
 
 function App() {
   return (
     <div className="App">
-        <Header/>
-        <Main/>
-        <Footer />
+    <Router> 
+      <Header />  
+      <Switch>
+        <Route path='/' component={() => <Landing /> } exact />
+        <Route path='/contact-us' component={() => <Contact /> } exact />
+      </Switch>
+      <Footer />
+    </Router>
+    
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,33 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import './header.css';
+import logo from '../../images/img/logo.png';
+
+// function header() {
+
+//   return <div className='header-container'>
+//     <div className='logo'>
+//         <img src ={logo} alt = 'logo' />
+//     </div>
+//     <div className='nav-links'>
+//         <ul>
+//             <li ><Link className='active' to = '/'>Home</Link></li>
+//             <li><Link to = '#'>Services</Link></li>
+//             <li><Link to = '#'>Blog</Link></li>
+//             <li><Link to = '#'>About us</Link></li>
+//             <li><Link to = '/contact-us'>Contact us</Link></li>
+//         </ul>
+//     </div>
+//   </div>;
+// }
+
+// export default header;
+
+
+
+
 import React,{ useState } from 'react';
+import { Link } from 'react-router-dom';
 import './header.css';
 import telegram from '../../images/telegram.png';
 import twitter from '../../images/download.png';
@@ -27,60 +56,32 @@ function Header() {
     }
     return (
         <div className = "header-container">
-            <div className = "logo left"><img src={logo1} alt='logo'/><p>Coin<span style={{color:'red'}}>Exporter</span></p></div>
+            <div className = "logo left"><img src={logo} alt='logo'/></div>
             <div className='toggler'>
                 <img src={searchIcon} alt='search icon' style={{marginRight:'20px'}} />
                {!open && <img src={hamburger_icon} alt='hamburger icon' className='hamburger-icon' onClick={toggleOpen} />}
-                {open && <img src={close_icon} alt = 'close icon' className='close-icon' onClick={toggleOpen} style={{filter: 'invert(1)', height: '22px'}}/>}
+                {open && <img src={close_icon} alt = 'close icon' className='close-icon' onClick={toggleOpen} style={{ height: '25px'}}/>}
             </div>
-            <div className='right'>
-            <div className = "coin">
-            <li className='coin'>Coins <KeyboardArrowDownIcon className='arrow down'/>
-                <ul className='coin-dropdown'>
-                    <li>Today's Best</li>
-                    <li>All Time Best</li>
-                    <li>New Listings</li>
-                    <li>By Marketcap</li>
-                    <li>Presales</li>
-                </ul>
-            </li>
+            <div className='right nav-links'>
+            
+              <ul>
+                  <li ><Link className='active' to = '/'>Home</Link></li>
+                  <li><Link to = '#'>Services</Link></li>
+                  <li><Link to = '#'>Blog</Link></li>
+                  <li><Link to = '#'>About us</Link></li>
+                  <li><Link to = '/contact-us'>Contact us</Link></li>
+              </ul>
+            
             </div>
-            <div className = "search-box">
-            <img src={searchIcon} alt='search icon'/>
-                <input type = 'text' placeholder = 'search coins...' />
-            </div>
-            <div className = "coin">
-            <li className='coin'>Account <KeyboardArrowDownIcon className='arrow down'/>
-                <ul className='coin-dropdown'>
-                    <li>Portfolio</li>
-                    <li>WatchList</li>
-                    <li>Login</li>
-                    <li>Register</li>
-                </ul>
-            </li>
-            </div>
-            <div className = "social-media">
-                <img src = {telegram} alt='telegram'/>
-                <img src = {twitter}  alt='twitter'/>
-            </div>
-            <div className = "submit-coin">  <button> <PublishIcon /> Submit Coin</button></div>
-            </div>
-            {  <div className='mobile-links' style={{left: left}}>
-                <ul>
-                    <li><Account className='icon' /> My Account</li>
-                    <li><Clock className='icon' />WatchList</li>
-                    <li> <Fire className='icon' /> Today's Best</li>
-                    <li><Clock className='icon' />All Time Best</li>
-                    <li><EmailIcon className='icon' />New Listings</li>
-                    <li><Fire className='icon' />By Marketcap</li>
-                    <li><Glass className='icon' />Presales</li>
-                    <li><PublishIcon className='icon'/>Submit Coin</li>
-                    <li><EmailIcon className='icon' /> Contact Us</li>
-                    <li> <img className='icon' src={twitter} alt = 'twitter'/> Follow us on Twitter</li>
-                    <li> <img className='icon' src={telegram} alt = 'telegram'/> Join our Telegram group! </li>
-                    
-                </ul>
-            </div>}
+            { <div className='mobile-links' style={{left: left}}>
+                  <ul onClick={ toggleOpen }>
+                    <li onClick={ toggleOpen }><Link className='active' to = '/'>Home</Link></li>
+                    <li onClick={ toggleOpen }><Link to = '#'>Services</Link></li>
+                    <li onClick={ toggleOpen }><Link to = '#'>Blog</Link></li>
+                    <li onClick={ toggleOpen }><Link to = '#'>About us</Link></li>
+                    <li onClick={ toggleOpen }><Link to = '/contact-us'>Contact us</Link></li>
+                  </ul>
+            </div> }
         </div>
     )
 }
