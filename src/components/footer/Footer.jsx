1020882e5ebeb587a6ca7849020de047dc805a0f
Isah Hamza <hamza@paritie.com>
2022-02-04 13:27:35 +0100

    making changes to the file system directories

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'
+
+function Footer() {
+  return <div className='footer-container'>
+    <p>
+        Copyright &copy; 2022 KadPay. All rights reserved
+    </p>
+  </div>;
+}
+
+export default Footer;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,89 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import './header.css';
+import logo from '../../images/img/logo.png';
+
+// function header() {
+
+//   return <div className='header-container'>
+//     <div className='logo'>
+//         <img src ={logo} alt = 'logo' />
+//     </div>
+//     <div className='nav-links'>
+//         <ul>
+//             <li ><Link className='active' to = '/'>Home</Link></li>
+//             <li><Link to = '#'>Services</Link></li>
+//             <li><Link to = '#'>Blog</Link></li>
+//             <li><Link to = '#'>About us</Link></li>
+//             <li><Link to = '/contact-us'>Contact us</Link></li>
+//         </ul>
+//     </div>
+//   </div>;
+// }
+
+// export default header;
+
+
+
+
+import React,{ useState } from 'react';
+import { Link } from 'react-router-dom';
+import './header.css';
+import telegram from '../../images/telegram.png';
+import twitter from '../../images/download.png';
+import searchIcon from '../../images/search-removebg.png';
+import logo1 from '../../images/rolling_coin.gif';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import PublishIcon from '@material-ui/icons/Publish';
+import hamburger_icon from '../../images/icon-hamburger.svg';
+import close_icon from '../../images/icon-close.svg';
+import EmailIcon from '@material-ui/icons/Email';
+// import UserIcon from '@material-ui/icons/Contact';
+import Account from '@material-ui/icons/AccountCircleSharp';
+import Clock from '@material-ui/icons/Timer';
+import Fire from '@material-ui/icons/FireplaceSharp';
+import Glass from '@material-ui/icons/HourglassEmpty'
+
+function Header() {
+    const [open, setOpen] = useState(false);
+    const [left, setLeft] = useState('-100%');
+    const toggleOpen = () => {
+        if(open)
+            setLeft('-110%');
+        else
+            setLeft('0%');
+        setOpen(!open);
+    }
+    return (
+        <div className = "header-container">
+            <div className = "logo left"><img src={logo} alt='logo'/></div>
+            <div className='toggler'>
+                <img src={searchIcon} alt='search icon' style={{marginRight:'20px'}} />
+               {!open && <img src={hamburger_icon} alt='hamburger icon' className='hamburger-icon' onClick={toggleOpen} />}
+                {open && <img src={close_icon} alt = 'close icon' className='close-icon' onClick={toggleOpen} style={{ height: '25px'}}/>}
+            </div>
+            <div className='right nav-links'>
+            
+              <ul>
+                  <li ><Link className='active' to = '/'>Home</Link></li>
+                  <li><Link to = '#'>Services</Link></li>
+                  <li><Link to = '#'>Blog</Link></li>
+                  <li><Link to = '#'>About us</Link></li>
+                  <li><Link to = '/contact-us'>Contact us</Link></li>
+              </ul>
+            
+            </div>
+            { <div className='mobile-links' style={{left: left}}>
+                  <ul onClick={ toggleOpen }>
+                    <li onClick={ toggleOpen }><Link className='active' to = '/'>Home</Link></li>
+                    <li onClick={ toggleOpen }><Link to = '#'>Services</Link></li>
+                    <li onClick={ toggleOpen }><Link to = '#'>Blog</Link></li>
+                    <li onClick={ toggleOpen }><Link to = '#'>About us</Link></li>
+                    <li onClick={ toggleOpen }><Link to = '/contact-us'>Contact us</Link></li>
+                  </ul>
+            </div> }
+        </div>
+    )
+}
+
+export default Header
